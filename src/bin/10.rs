9fa3e83c0db9584f0abd1ddccd41d033a48f737a
Oscar Cederberg <os7138ce-s@student.lu.se>
2025-12-11 05:56:31 +0100

    Fix day 10

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -1,78 +1,80 @@
-use memoize::memoize;
+use std::collections::{HashSet, VecDeque};
+
 use z3::{Optimize, ast::Int};
 
 const TEST: &str = include_str!("../../input/10/test.txt");
 const INPUT: &str = include_str!("../../input/10/input.txt");
 
 pub struct Machine {
-    pub lights_req: Vec<bool>,
+    pub light_diag: Vec<bool>,
+    pub button_schemas: Vec<Vec<usize>>,
     pub joltage_req: Vec<usize>,
-    pub buttons: Vec<Vec<usize>>,
 }
 
-#[memoize]
-fn solve_lights_req(
-    goal: Vec<bool>,
-    current: Vec<bool>,
-    buttons: Vec<Vec<usize>>,
-    iter: usize,
-) -> usize {
-    /* TODO: Make this BFS instead of DFS with memoization. */
-    if goal == current || iter > 10 {
-        return iter;
+fn solve_lights_req(goal: Vec<bool>, schemas: Vec<Vec<usize>>) -> usize {
+    let mut seen: HashSet<Vec<bool>> = HashSet::new();
+    let mut queue: VecDeque<(Vec<bool>, usize)> = VecDeque::new();
+
+    queue.push_back((vec![false; goal.len()], 0));
+
+    while let Some((state, iter)) = queue.pop_front() {
+        if state == goal {
+            return iter;
+        } else if seen.contains(&state) {
+            continue;
+        }
+
+        seen.insert(state.clone());
+
+        schemas.iter().for_each(|button| {
+            let mut next = state.clone();
+            button.iter().for_each(|&index| next[index] = !next[index]);
+            queue.push_back((next, iter + 1));
+        })
     }
 
-    buttons
-        .iter()
-        .map(|button| {
-            let mut next = current.clone();
-            button.iter().for_each(|&index| next[index] = !next[index]);
-            solve_lights_req(goal.clone(), next, buttons.clone(), iter + 1)
-        })
-        .min()
-        .unwrap()
+    panic!();
 }
 
-fn solve_joltage_req(goal: Vec<usize>, buttons: Vec<Vec<usize>>) -> usize {
+fn solve_joltage_req(goal: Vec<usize>, schemas: Vec<Vec<usize>>) -> usize {
     let opt = Optimize::new();
 
-    let constants: Vec<(&[usize], Int)> = buttons
+    let schemas: Vec<Vec<usize>> = schemas
         .iter()
-        .enumerate()
-        .map(|(index, button)| {
-            (
-                button.as_slice(),
-                Int::fresh_const(format!("{index}").as_str()),
-            )
+        .map(|schema| {
+            let mut delta: Vec<usize> = vec![0; goal.len()];
+            schema.iter().for_each(|&index| delta[index] = 1);
+            delta
         })
+        .collect();
+
+    let constants: Vec<Int> = (0..schemas.len())
+        .map(|index| Int::fresh_const(format!("{index}").as_str()))
         .collect();
 
     for index in 0..goal.len() {
         let muls: Vec<Int> = constants
             .iter()
-            .map(|(button, constant)| Int::mul(&[constant, &Int::from_u64(button[index] as u64)]))
+            .zip(schemas.iter())
+            .map(|(constant, schema)| Int::mul(&[constant, &Int::from_u64(schema[index] as u64)]))
             .collect();
 
         opt.assert(&Int::add(&muls).eq(Int::from_u64(goal[index] as u64)))
     }
 
-    for constant in constants.iter() {
-        opt.assert(&constant.1.ge(Int::from_u64(0)));
-    }
+    constants.iter().for_each(|constant| {
+        opt.assert(&constant.ge(Int::from_u64(0)));
+    });
 
-    let terms: Vec<&Int> = constants.iter().map(|(_, constant)| constant).collect();
-    let objective = Int::add(&terms);
+    let objective = Int::add(&constants);
     opt.minimize(&objective);
 
-    if opt.check(&[]) == z3::SatResult::Sat {
-        let model = opt.get_model().unwrap();
-        constants
-            .iter()
-            .map(|(_, constant)| model.get_const_interp(constant).unwrap().as_u64().unwrap())
-            .sum::<u64>() as usize
-    } else {
-        panic!();
-    }
+    assert!(opt.check(&[]) == z3::SatResult::Sat);
+    let model = opt.get_model().unwrap();
+    constants
+        .iter()
+        .map(|constant| model.get_const_interp(constant).unwrap().as_u64().unwrap())
+        .sum::<u64>() as usize
 }
 
 fn part_1(input: &str) -> usize {
@@ -80,7 +82,7 @@ fn part_1(input: &str) -> usize {
         .lines()
         .map(|line| {
             let parts: Vec<&str> = line.split(' ').collect();
-            let lights_req: Vec<bool> = parts[0][1..parts[0].len() - 1]
+            let diagram: Vec<bool> = parts[0][1..parts[0].len() - 1]
                 .chars()
                 .map(|light| match light {
                     '.' => false,
@@ -88,33 +90,32 @@ fn part_1(input: &str) -> usize {
                     other => unreachable!("parsed unknown char: {other}"),
                 })
                 .collect();
-            let buttons = parts[1..parts.len() - 1]
+            let schematic = parts[1..parts.len() - 1]
                 .iter()
-                .map(|schematic| {
-                    schematic[1..schematic.len() - 1]
+                .map(|schema| {
+                    schema[1..schema.len() - 1]
                         .split(',')
                         .map(|light| light.parse().unwrap())
                         .collect()
                 })
                 .collect();
+            let requirement: Vec<usize> = parts[parts.len() - 1]
+                [1..parts[parts.len() - 1].len() - 1]
+                .split(',')
+                .map(|joltage| joltage.parse().unwrap())
+                .collect();
+
             Machine {
-                lights_req,
-                joltage_req: vec![],
-                buttons,
+                light_diag: diagram,
+                button_schemas: schematic,
+                joltage_req: requirement,
             }
         })
         .collect();
 
     machines
         .iter()
-        .map(|machine| {
-            solve_lights_req(
-                machine.lights_req.clone(),
-                vec![false; machine.lights_req.len()],
-                machine.buttons.clone(),
-                0,
-            )
-        })
+        .map(|machine| solve_lights_req(machine.light_diag.clone(), machine.button_schemas.clone()))
         .sum()
 }
 
@@ -123,32 +124,42 @@ fn part_2(input: &str) -> usize {
         .lines()
         .map(|line| {
             let parts: Vec<&str> = line.split(' ').collect();
-            let joltage_req: Vec<usize> = parts[parts.len() - 1]
-                [1..parts[parts.len() - 1].len() - 1]
-                .split(',')
-                .map(|req| req.parse().unwrap())
-                .collect();
-            let buttons = parts[1..parts.len() - 1]
-                .iter()
-                .map(|schematic| {
-                    let mut button: Vec<usize> = vec![0; joltage_req.len()];
-                    schematic[1..schematic.len() - 1]
-                        .split(',')
-                        .for_each(|light| button[light.parse::<usize>().unwrap()] += 1);
-                    button
+            let diagram: Vec<bool> = parts[0][1..parts[0].len() - 1]
+                .chars()
+                .map(|light| match light {
+                    '.' => false,
+                    '#' => true,
+                    other => unreachable!("parsed unknown char: {other}"),
                 })
                 .collect();
+            let schematic = parts[1..parts.len() - 1]
+                .iter()
+                .map(|schema| {
+                    schema[1..schema.len() - 1]
+                        .split(',')
+                        .map(|light| light.parse().unwrap())
+                        .collect()
+                })
+                .collect();
+            let requirement: Vec<usize> = parts[parts.len() - 1]
+                [1..parts[parts.len() - 1].len() - 1]
+                .split(',')
+                .map(|joltage| joltage.parse().unwrap())
+                .collect();
+
             Machine {
-                lights_req: vec![],
-                joltage_req,
-                buttons,
+                light_diag: diagram,
+                button_schemas: schematic,
+                joltage_req: requirement,
             }
         })
         .collect();
 
     machines
         .iter()
-        .map(|machine| solve_joltage_req(machine.joltage_req.clone(), machine.buttons.clone()))
+        .map(|machine| {
+            solve_joltage_req(machine.joltage_req.clone(), machine.button_schemas.clone())
+        })
         .sum()
 }
 
